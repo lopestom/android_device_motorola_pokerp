2790f0e341eabb12c51d7877b90f286ac55abe7e
lopestorm <lopestom1@gmail.com>
2021-11-24 13:34:11 -0300

    Update common_mt676_.mk

--- MTK/common_mt676_/common_mt676_.mk
+++ MTK/common_mt676_/common_mt676_.mk
@@ -1,12 +1,12 @@
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+#$(call inherit-product, vendor/omni/config/common.mk)
 
 # enable stock zip packages flash
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+#PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.treble.enabled=true \
     persist.sys.usb.config=mtp \
     persist.service.adb.enable=1 \
